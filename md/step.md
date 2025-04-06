388015af08be1a6ea43a501e6fbc6d5f2527be77
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-06 09:15:11 +0700

    a store and useStore

---
+++ md/step.md
@@ -0,0 +1,8 @@
+# Step Zustand
+
+[Docs](https://zustand.docs.pmnd.rs/getting-started/introduction)
+
+1. npm install zustand
+2. create a store folder and file
+3. import { create } from 'zustand'and build function
+4. in the page, import { userStore } from "@/app/store/user"
---
+++ src/app/store/user.tsx
@@ -0,0 +1,30 @@
+import { create } from 'zustand'
+
+export const userStore = create((set) => ({
+    user: {
+        full_name: "Sahasawat R",
+        short_name: "Boss"
+    }
+}));
+
+// "use client"
+
+// import { userStore } from "@/app/store/user"
+
+// const Home = () => {
+
+//     const user = userStore((state: any) => state.user)
+
+//     return (
+//         <main>
+//             Home
+//             <div>
+//                 {user.full_name}
+//                 <br />
+//                 {user.short_name}
+//             </div>
+//         </main>
+//     )
+// }
+
+// export default Home
